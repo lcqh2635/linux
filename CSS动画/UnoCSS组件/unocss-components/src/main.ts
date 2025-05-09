5921bd6603d8f324c7044724db20ace5adaca545
龙茶清欢 <2320391937@qq.com>
2025-05-09 18:26:24 +0800

    backup

--- CSS动画/UnoCSS组件/unocss-components/src/main.ts
+++ CSS动画/UnoCSS组件/unocss-components/src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import '@assets/styles/index.scss'
-import App from './App.vue'
+import App from '@/App.vue'
 
-createApp(App).mount('#app')
+createApp(App)
+    .mount('#app')
