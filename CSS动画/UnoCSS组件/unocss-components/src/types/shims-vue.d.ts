9893b3881c58e5bc9a269e4ce424d80b034e2dfa
龙茶清欢 <2320391937@qq.com>
2025-05-10 18:42:09 +0800

    backup

--- CSS动画/UnoCSS组件/unocss-components/src/types/shims-vue.d.ts
+++ CSS动画/UnoCSS组件/unocss-components/src/types/shims-vue.d.ts
@@ -9,24 +9,18 @@
 
 // 声明一个模块，用于匹配所有以 ".vue" 结尾的文件，并将其识别为一个 Vue 组件
 declare module '*.vue' {
-    // 从 "vue" 中导入 DefineComponent 类型
-    import type { DefineComponent } from 'vue'
-    // 定义一个类型为 DefineComponent 的变量 component
-    // 它具有三个泛型参数，分别表示组件的 props、组件的 data 和其他的类型。
-    // 在这里，我们使用空对象（{}）表示没有 props，使用空对象（{}）表示没有 data，使用 any 表示其他类型可以是任意值。
-    const component: DefineComponent<{}, {}, any>
+    import type { ComponentOptions } from 'vue'
+    const Component: ComponentOptions
     // 默认导出
     // import Doc from '../doc.md'  // 直接导入默认导出，Doc 就是 component
-    export default component
+    export default Component
 }
+
 // 新增 Markdown 模块声明（作为 Vue 组件，可以像普通 Vue 组件一样使用 ）
 declare module '*.md' {
-    import type { DefineComponent } from 'vue'
-    const component: DefineComponent<{}, {}, any>
-    // 命名导出
-    // import { component as Doc } from '../doc.md'  // 必须解构命名导出
-    export { component }
-    // export default component
+    import type { ComponentOptions } from 'vue'
+    const Component: ComponentOptions
+    export default Component
 }
 
 
