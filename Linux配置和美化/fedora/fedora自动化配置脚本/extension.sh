e9d67a0d6648ccc46fa27e142eb127b62d1be8ee
龙茶清欢 <2320391937@qq.com>
2025-07-04 11:20:17 +0800

    vault backup: 2025-07-04 11:20:17

--- Linux配置和美化/fedora/fedora自动化配置脚本/extension.sh
+++ Linux配置和美化/fedora/fedora自动化配置脚本/extension.sh
@@ -105,6 +105,8 @@ Bluetooth Battery Meter
 Astra Monitor
 Applications Overview Tooltip
 Hide Universal Access
+Customize Clock on Lock Screen
+Window Centering
 
 
 nautilus ~/.local/share/gnome-shell/extensions
